811eef52401962731f387943e257bc9ec7ac6a78
SCBee <77908243+SCBee@users.noreply.github.com>
2021-03-10 16:44:39 -0700

    March10

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@
 
 int main(int, char**)
 {
-    // Call driving functions
-    motor.driver();
-    cable.driver();
-
-    // Output
-    motor.output();
-    cable.output();
+    
 
     return 0;
 }
